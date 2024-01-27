fd609c2d84d672c6687b91cd0e6e924103020377
Samuel <wongsamuel12321@gmail.com>
2024-01-27 14:12:22 +0800

    submit

--- main.py
+++ main.py
@@ -3,41 +3,64 @@
 Q1: 
 '''
 
-# TODO: Write your code here
+def greet(name):
+   print(f'Hi, {name}!')
+
+greet('Alice')
+greet('Bob')
 
 ##################################################
 '''
 Q2:
 '''
 
-# TODO: Write your code here
+def greet(name1, name2):
+   print(f'Hi, {name1} and {name2}')
+
+
+greet(name2='Bob', name1='Alex')
 
 ##################################################
 '''
 Q3:
 '''
 
-# TODO: Write your code here
+def find_rect_area(width, height):
+    print(f'the rectangle  area is {width * height}')
+
+
+find_rect_area(width=12.3, height=45.6)
 
 ##################################################
 '''
 Q4:
 '''
 
-# TODO: Write your code here
+def bought_and_fed(animal):
+    print(f'Bought me a {animal} and the {animal} pleased me, I fed my {animal} under yonder tree.', end=' ')
+    
+def fiddle_cat():
+    print('Cat goes fiddle-i-fee.', end='\n')
 
-##################################################
-'''
-Q5:
-'''
+def hen_chimmy_chuck():
+    print('Hen goes chimmy-chuck, chimmy-chuck,', end=' ')
+    fiddle_cat()
 
-# TODO: Write your code here
+def duck_quack():
+    print('Duck goes quack, quack,', end=' ')
+    hen_chimmy_chuck()
 
-##################################################
-'''
-Q6:
-'''
+def goose_hissy():
+    print('Goose goes hissy, hissy,', end=' ')
+    duck_quack()
 
-# TODO: Write your code here
+bought_and_fed('cat')
+fiddle_cat()
+bought_and_fed('hen')
+hen_chimmy_chuck()
+bought_and_fed('duck')
+duck_quack()
+bought_and_fed('goose')
+goose_hissy()
 
 ##################################################
